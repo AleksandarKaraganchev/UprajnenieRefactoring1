70b29a070e0867d47b2798cfd9e15ea19ca96f22
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2024-12-14 15:49:35 +0200

    Add project files.

---
+++ Zad1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public static class Calculator
+    {
+        public static void Comparator(int x, int y)
+        {
+            if (x == y)
+            {
+                Console.WriteLine("Equal (X = Y)");
+            }
+            else if (x > y)
+            {
+                Console.WriteLine("Greater (X > Y)");
+            }
+            else
+            {
+                Console.WriteLine("Lesser (X < Y)");
+            }
+        }
+    }
+}
---
+++ Zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter y: ");
+            int y = int.Parse(Console.ReadLine());
+            Calculator.Comparator(x, y);
+        }
+    }
+}
